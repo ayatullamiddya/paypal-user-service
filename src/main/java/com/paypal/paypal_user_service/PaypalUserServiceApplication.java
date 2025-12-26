7f917197c8f900983cea61607bd51fd12dccfa49
ayatullamiddya <ayatullamiddya99@gmail.com>
2025-12-27 01:38:51 +0530

    first commit

---
+++ src/main/java/com/paypal/paypal_user_service/PaypalUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paypal.paypal_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paypal/paypal_user_service/PaypalUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypal.paypal_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
